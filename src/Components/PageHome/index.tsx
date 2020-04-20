920e3ae42646bed0f075ce192262ba5b614792a7
dwd-umd <ddarg@umd.edu>
2020-04-20 00:14:16 -0400

    Add script to deploy to github pages

--- src/Components/PageHome/index.tsx
+++ src/Components/PageHome/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const PageHome = () => {
   return (
     <>
-      <h1>Welcome!</h1>
+      <h1>Wilkommen!!</h1>
       <p>This is the home page!</p>
     </>
   );
