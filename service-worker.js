7bc4ff2e92c1e4ca7a14e5ff8f215b83a0b75ca9
dwd-umd <ddarg@umd.edu>
2020-04-20 10:06:11 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pdco-dashboard/precache-manifest.beb587a893358c7454673755f77e4647.js"
+  "/pdco-dashboard/precache-manifest.469de490c1d88609e0d3e82e8f483ce6.js"
 );
 
 self.addEventListener('message', (event) => {
