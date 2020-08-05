2c7634adff11d3c9ae880ffcf663ee1ec8228488
dan-drago <ddarg@umd.edu>
2020-08-05 08:14:59 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pdco-dashboard/precache-manifest.1b6580b83103107323cb2785eded302b.js"
+  "/pdco-dashboard/precache-manifest.a1ba3b5a05c4ec26ec7c7da02e2ae48b.js"
 );
 
 self.addEventListener('message', (event) => {
