1aeb5b6c6da54615c8d2d5c83ec27f7603c62951
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2021-08-02 12:38:18 -0700

    Added menu for list of sites

--- src/Components/ProgramsMap/index.tsx
+++ src/Components/ProgramsMap/index.tsx
@@ -4,11 +4,13 @@ import { useStyles } from './styles';
 import { smallMapIcons, largeMapIcons } from './icons';
 import { Dialog, DialogProps } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faAlignJustify, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 export const ProgramsMap = () => {
   const classes = useStyles();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
       <div className={classes.container}>
@@ -34,6 +36,31 @@ export const ProgramsMap = () => {
           open={isDialogOpen}
         >
           <div className={classes.dialogContainer}>
+            <div
+              className={classes.menuContainer}
+              style={{
+                left: `${isMenuOpen ? 0 : -300}px`
+              }}
+            >
+              <div className={classes.menuHeader}>
+                <p>Sites</p>
+
+                <div className={classes.closeButton} onClick={() => setIsMenuOpen(false)}>
+                  <FontAwesomeIcon icon={faTimes} />
+                </div>
+              </div>
+
+              {largeMapIcons.map((item, index) => (
+                <div className={classes.menuRow}>
+                  <p>{item.iconLabel}</p>
+
+                  <a href={item.iconLink} target="_blank" rel="noopener noreferrer">
+                    {item.iconLink}
+                  </a>
+                </div>
+              ))}
+            </div>
+
             <div className={classes.dialogMapWrapper}>
               <WorldDaylightMap
                 options={{
@@ -44,6 +71,10 @@ export const ProgramsMap = () => {
                   // icons: smallMapIcons.concat(largeMapIcons)
                 }}
               />
+            </div>
+
+            <div className={classes.menuButton} onClick={() => setIsMenuOpen(true)}>
+              <FontAwesomeIcon icon={faAlignJustify} />
             </div>
 
             <div className={classes.closeButton} onClick={() => setIsDialogOpen(false)}>
--- src/Components/ProgramsMap/styles.ts
+++ src/Components/ProgramsMap/styles.ts
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core';
+import { mobileWidthPxl } from '../../Utils/constants';
 
 //
 export const useStyles = makeStyles(
@@ -37,7 +38,76 @@ export const useStyles = makeStyles(
       right: 0,
 
       '& > div > div': {
-        margin: '0 60px 0 0'
+        margin: '0 60px 0 60px'
+      }
+    },
+    menuContainer: {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+
+      display: 'flex',
+      flexDirection: 'column',
+      overflowY: 'auto',
+      overflowX: 'hidden',
+
+      width: '25%',
+      minWidth: '250px',
+      maxWidth: '300px',
+      height: '100%',
+
+      background: '#181b2e',
+      zIndex: 10,
+
+      transition: 'ease-in-out 0.12s'
+    },
+    menuHeader: {
+      display: 'flex',
+      flexDirection: 'row',
+      margin: '0 0 1rem 0',
+
+      '& p': {
+        fontSize: '1.5rem',
+        fontWeight: 600,
+        margin: '1rem'
+      }
+    },
+    menuRow: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+
+      margin: '0 1rem 1.5rem 1rem',
+
+      '& p, a': {
+        color: 'white',
+        textDecoration: 'none',
+
+        margin: 0
+      },
+      '& a': {
+        textDecoration: 'underline',
+        wordBreak: 'break-all',
+        color: 'rgba(255, 255, 255, 0.7)'
+      }
+    },
+    menuButton: {
+      position: 'absolute',
+      top: '10px',
+      left: '10px',
+      width: '40px',
+      height: '40px',
+      border: 'solid 1px rgba(255, 255, 255, 0.1)',
+
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+
+      transition: 'background-color 0.2s ease-in-out',
+      cursor: 'pointer',
+      '&:hover': {
+        background: 'rgba(255, 255, 255, 0.3)'
       }
     },
     closeButton: {
@@ -81,6 +151,20 @@ export const useStyles = makeStyles(
       // backgroundSize: '100% 100%',
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center center'
+    },
+
+    [`@media (max-width: ${mobileWidthPxl}px)`]: {
+      dialogMapWrapper: {
+        '& > div > div': {
+          margin: '0'
+        }
+      },
+      menuButton: {
+        display: 'none'
+      },
+      closeButton: {
+        display: 'none'
+      }
     }
   }),
 
