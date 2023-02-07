b518c15887bd73f8e61d56e079d5a40086a4fad0
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2023-02-07 14:09:16 -0800

    Delete duplicate Utils directory

--- src/Utils/formattedTime.ts
+++
@@ -1,8 +0,0 @@
-import { MONTHS } from './constants';
-
-export const formattedTimestamp = (timestamp: string): string => {
-  const d = new Date(timestamp);
-  const date = `${MONTHS[d.getMonth()]}-${d.getDate()}-${d.getFullYear()}`;
-  const time = d.toTimeString().split(' ')[0];
-  return `${time} ${date}`;
-};
