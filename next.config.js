1cd96f6fd6261db81ea2cb8f49ce6687e87883d0
d-w-d <ddarg@umd.edu>
2023-02-12 08:46:02 -0500

    Bookmark before @next/codemod

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { esmExternals: true },
-  distDir: 'build'
+  // experimental: { esmExternals: true },
+  distDir: "build",
 };
 
 module.exports = nextConfig;
